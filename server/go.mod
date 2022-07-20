e58e1503e4b705324a921f71fe4f1c67bd636678
Siddhant Prateek <siddhantprateek@gmail.com>
2022-07-20 11:49:41 +0530

    api modules error need fixes

--- server/go.mod
+++ server/go.mod
@@ -7,7 +7,6 @@ require github.com/joho/godotenv v1.4.0
 require github.com/rs/cors v1.8.2
 
 require (
-	github.com/99designs/gqlgen v0.17.13 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
@@ -24,11 +23,12 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/tools v0.1.11-0.20220513221640-090b14e8501f // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	honnef.co/go/tools v0.3.2
 )
--- server/main.go
+++ server/main.go
@@ -6,8 +6,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/example/federation/accounts/graph"
+	"github.com/99designs/gqlgen/example/federation/products/graph/generated"
+	"github.com/99designs/gqlgen/example/federation/reviews/graph/generated"
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/rs/cors"
+	"honnef.co/go/tools/analysis/facts/generated"
 )
 
 func main() {
@@ -17,12 +22,16 @@ func main() {
 		port = "4000"
 		log.Printf("defaulting to port %s", port)
 	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/news", get_news)
 	mux.HandleFunc("/", running)
 	mux.HandleFunc("/api/topmonth", top_nft_this_month)
-	mux.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 	mux.HandleFunc("/api/artist", get_nft_artist)
+	mux.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+	mux.Handle("/graphql", srv)
 	handler := cors.Default().Handler(mux)
 	fmt.Printf("Server running at \n http://localhost:4000/api/ \n http://localhost:4000/api/artist \n http://localhost:4000/api/news")
 	if err := http.ListenAndServe(":"+port, handler); err != nil {
